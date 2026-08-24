4f1fa7cd35ece8507949a99ea79367b2f418284e
aman chamoli <amanchamoli789@gmail.com>
2026-08-24 17:52:03 +0530

    "DB connected"

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
-import connectDB from "./db";
-require('dotenv').config()
+import dns from "node:dns";
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
+dotenv.config({
+    path: './.env'
+});
 
 connectDB()
