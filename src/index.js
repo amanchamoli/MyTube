79f0e53248961570902013aae8a08cb35b411260
aman chamoli <amanchamoli789@gmail.com>
2026-08-28 22:22:28 +0530

    "ADD route and controllers"

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import dns from "node:dns";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
+import { app } from "./app.js";
 
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
@@ -11,7 +12,7 @@ dotenv.config({
 connectDB()
 .then( () => {
     app.listen(process.env.PORT || 8000, () => {
-        console.log(`Server running on port ${process.env.PORT}`);
+        console.log(`Server running on port ${process.env.PORT || 8000}`);
     })
 })
 .catch((err)=> {
