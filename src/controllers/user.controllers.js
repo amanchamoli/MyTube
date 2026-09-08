a097ff5b66e163f09fd5577a445a253500668dd0
aman chamoli <amanchamoli789@gmail.com>
2026-09-08 22:39:43 +0530

    "fixed" login in usercontoller"

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -88,9 +88,16 @@ const loginUser = asyncHandler(async (req,res) =>{
 
     const { username, email, password} = req.body
 
-    if(!username || !email){
-        throw new ApiError(400, "All fields are required")
+    if (!username && !email) {
+        throw new ApiError(400, "username or email is required")
     }
+    
+    // Here is an alternative of above code based on logic discussed in video:
+    // if (!(username || email)) {
+    //     throw new ApiError(400, "username or email is required")
+        
+    // }
+
 
     const user = await User.findOne({
         $or: [{username}, {email}]
